41dfe08b250317f9f20660706d53f913ef43a304
Николай <kalibas1@mail.ru>
2022-05-17 20:37:57 +0300

    first commit

--- app/src/main/java/com/example/netologiproject/MainActivity.kt
+++ app/src/main/java/com/example/netologiproject/MainActivity.kt
@@ -3,30 +3,96 @@ package com.example.netologiproject
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import android.widget.TextView
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.*
+import com.google.android.material.appbar.MaterialToolbar
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.textfield.TextInputEditText
 
 class MainActivity : AppCompatActivity() {
 
     val TAG: String = "MainActivity"
 
+    lateinit var requestInput: TextInputEditText
+
+    lateinit var podsAdapter: SimpleAdapter
+
+    lateinit var progressBar: ProgressBar
+
+    val pods = mutableListOf<HashMap<String, String>>(
+        HashMap<String, String>().apply {
+            put("Title", "Title 1")
+            put("Content", "Content 1")
+        },
+        HashMap<String, String>().apply {
+            put("Title", "Title 2")
+            put("Content", "Content 2")
+        },
+        HashMap<String, String>().apply {
+            put("Title", "Title 3")
+            put("Content", "Content 3")
+        },
+        HashMap<String, String>().apply {
+            put("Title", "Title 4")
+            put("Content", "Content 4")
+        }
+    )
+
     override fun onCreate(savedInstanceState: Bundle?) {
         Log.d(TAG, "start of onCreate function")
-
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val name: String = "Ivan"
-        val surname: String = "Ivanov"
-        val age: Int = 37
-        val height: Double = 172.2
+        initViews()
+    }
 
-        val stringOfText = "name: $name, surname: $surname, age: $age, height: $height"
+    fun initViews() {
+        val toolbar: MaterialToolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
 
-        val output: TextView = findViewById(R.id.output)
-        output.text = stringOfText
+        requestInput = findViewById(R.id.text_input_edit)
 
-        Log.d(TAG, "end of onCreate function")
+        val podsList: ListView = findViewById(R.id.pods_list)
+
+        podsAdapter = SimpleAdapter(
+            applicationContext,
+            pods,
+            R.layout.item_pod,
+            arrayOf("Title", "Content"),
+            intArrayOf(R.id.title, R.id.content)
+        )
+        podsList.adapter = podsAdapter
+
+        val voiceInputButton: FloatingActionButton = findViewById(R.id.voice_input_button)
+        voiceInputButton.setOnClickListener {
+            Log.d(TAG, "FAB")
+        }
+
+        progressBar = findViewById(R.id.progress_bar)
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.toolbar_menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.action_stop -> {
+                Log.d(TAG, "action stop")
+                return true
+            }
+            R.id.action_clear -> {
+                Log.d(TAG, "action clear")
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
+
+
 
 
 
